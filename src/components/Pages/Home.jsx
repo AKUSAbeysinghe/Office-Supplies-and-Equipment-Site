32ef036dacc279b88fb1e22d44b454cee2ed3a47
AKUSAbeysinghe <umeshasewwandi420@gmail.com>
2026-06-28 01:32:16 +0530

    Backend Initializing

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 
-import Hero from "../Hero";
-import CategoryGrid from "../CategoryGrid";
-import Collections from "../CollectionsSection";
-import WhyChoose from "./WhyChoose";
-import TestimonialsSection from "./Testimonials";
-import CTABanner from "../CTABanner";
+import Hero from "../Hero.jsx";
+import CategoryGrid from "../CategoryGrid.jsx";
+import Collections from "../CollectionsSection.jsx";
+import WhyChoose from "./WhyChoose.jsx";
+import TestimonialsSection from "./Testimonials.jsx";
+import CTABanner from "../CTABanner.jsx";
 
 const Home = () => {
   return (
